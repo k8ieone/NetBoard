7fa8eb3ae71bfb874b54b0f1e49fd822b3b2c2f8
k8ie <k8ie@mcld.eu>
2020-06-18 22:53:46 +0200

    work plssssssssssss

--- magic.js
+++ magic.js
@@ -4,7 +4,9 @@ function readTheConfigFile() {
     // let configFile = fetch("config.json").then(Response => Response.json()).then(jsonResponse);
     // console.log(configFile);
     //return configFile
-    let configFile = $.getJSON("config.json", function(data){})
+    let configFile = $.getJSON("config.json", function(data){
+        return data.responseJSON;
+    })
     console.log(configFile.responseJSON);
     return configFile.responseJSON;
 }
