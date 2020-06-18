a1a1b111f7f20760227cbf8b6e32d63b30d52145
k8ie <k8ie@mcld.eu>
2020-06-18 15:23:41 +0200

    Initial concept

--- magic.js
+++ magic.js
@@ -1,1 +1,11 @@
 // this is where the magic should happen...
+
+function readTheConfigFile() {
+    let configFile = fetch("config.json").then(Response => Response.json()).then(jsonResponse => console.log(jsonResponse))
+}
+
+function buildTheWebsite() {
+    return 0;
+}
+
+readTheConfigFile();
