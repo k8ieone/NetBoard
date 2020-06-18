815158596a9b8c164077b37785354b56d711985c
satcom886 <rostik.medved@gmail.com>
2020-06-18 23:57:52 +0200

    improved readability

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 let configFile = {
     // jsSource": "https://pifour1-netdata.mcld.eu/dashboard.js",
     // ^^^^^^^^^^ this is actually not used for now
-    "groups": [ {"groupName": "Group 1",
-                "servers":[
-                    {"serverName": "pifour1",
-                    "serverURL": "https://pifour1-netdata.mcld.eu/"},
-                    {"serverName": "pithree1",
-                    "serverURL": "https://pithree1-netdata.mcld.eu/"}]},
-                {"groupName":"Group 2",
-                "servers":[
-                    {"serverName": "pifour2",
-                    "serverURL": "https://pifour2-netdata.mcld.eu/"}]}]
-}
+    groups: [ {groupName: "Group 1",
+                servers:[
+                    {serverName: "pifour1", serverURL: "https://pifour1-netdata.mcld.eu/"},
+                    {serverName: "pithree1", serverURL: "https://pithree1-netdata.mcld.eu/"}
+                ]},
+                {groupName:"Group 2",
+                servers:[
+                    {serverName: "pifour2", serverURL: "https://pifour2-netdata.mcld.eu/"}]
+                }]
+};
