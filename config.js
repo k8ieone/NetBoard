a36a4be7a213c42ca6d27e8f92569e91d3d7f8f0
satcom886 <rostik.medved@gmail.com>
2020-06-18 23:30:52 +0200

    Now it works but is still a little broken

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-{
+let configFile = {
     "jsSource": "https://pifour1-netdata.mcld.eu/dashboard.js",
     "groups": [{"groupName": "Group 1", "servers":
                 [{"serverName": "pifour1", "serverURL": "https://pifour1-netdata.mcld.eu/"},
