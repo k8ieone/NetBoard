0c6431cd40f5250d6483c5893b74c54064b666b1
satcom886 <rostik.medved@gmail.com>
2020-06-18 23:49:39 +0200

    Giving up on the idea of reading the dashboard URL from a config file for now

--- magic.js
+++ magic.js
@@ -1,9 +1,9 @@
 // this is where the magic should happen...
 
 function buildTheWebsite() {
-    // for (group in configFile.)
-    console.log("The source for dashboard.js should now be set to:");
-    console.log(configFile.jsSource);
+    for (group in configFile.groups){
+        
+    }
 }
 
 buildTheWebsite();
