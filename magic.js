284c67ea1d1bf664d38ed807af485f60ad343148
k8ie <k8ie@mcld.eu>
2020-06-18 23:01:43 +0200

    THIS SHOULD HAVE BEEN THE EASIEST PART

--- magic.js
+++ magic.js
@@ -1,22 +1,23 @@
 // this is where the magic should happen...
 
+let configFile = $.getJSON("config.json", function(data){
+    return data.responseJSON;
+})
+
 function readTheConfigFile() {
     // let configFile = fetch("config.json").then(Response => Response.json()).then(jsonResponse);
     // console.log(configFile);
     //return configFile
-    let configFile = $.getJSON("config.json", function(data){
-        return data.responseJSON;
-    })
     console.log(configFile.responseJSON);
-    return configFile.responseJSON;
+    return configFile;
 }
 
 function buildTheWebsite() {
     // for (group in configFile.)
-    let configFile = readTheConfigFile();
+    // let configFile = readTheConfigFile();
     document.getElementById("jsSource").src = configFile.jsSource;
     console.log("The source for dashboard.js should now be set to:");
     console.log(configFile.jsSource);
 }
 
-buildTheWebsite();
+readTheConfigFile();
