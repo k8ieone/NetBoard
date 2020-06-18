46d22ae074b45a3564588d6718b000720df6f24e
k8ie <k8ie@mcld.eu>
2020-06-18 16:26:02 +0200

    how 'bout now

--- magic.js
+++ magic.js
@@ -3,15 +3,15 @@
 function readTheConfigFile() {
     let configFile;
     fetch("config.json").then(Response => Response.json()).then(jsonResponse = configFile);
-    return configFile;
+    return JSON.parse(configFile);
 }
 
 function buildTheWebsite() {
     // for (group in configFile.)
     configFile = readTheConfigFile();
+    document.getElementById("jsSource").src = configFile.jsSource;
     console.log("The source for dashboard.js should now be set to:");
     console.log(configFile.jsSource);
-    document.getElementById("jsSource").src = configFile.jsSource;
 }
 
 buildTheWebsite();
