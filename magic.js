393d0f9f28971a29c9cc9b5c4e9faa758ddb443b
satcom886 <rostik.medved@gmail.com>
2020-06-19 00:54:11 +0200

    Initial WORKING concept of reading the config

--- magic.js
+++ magic.js
@@ -1,9 +1,27 @@
 // this is where the magic should happen...
 
 function buildTheWebsite() {
-    for (group in configFile.groups){
-        
+    let group
+    for (group of configFile.groups){
+        drawGroup(group.groupName);
+        for (server of group.servers){
+            drawServer(server.serverName, server.serverURL);
+        }
     }
+}
+
+function drawGroup(groupName){
+    console.log(groupName);
+    //let serverGroupDiv = document.createElement("div");
+    //serverGroupDiv.classList.add("w3-container");
+    // configFile.groups[0].groupName = "Group 1"
+    //serverGroupDiv.id = group.
+    //document.getElementById("body").appendChild(serverGroupDiv);
+}
+
+function drawServer(serverName, serverURL){
+    console.log(serverName);
+    console.log(serverURL);
 }
 
 buildTheWebsite();
