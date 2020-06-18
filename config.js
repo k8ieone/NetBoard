034a754face2f820e6438bc41829b4c164a86402
k8ie <k8ie@mcld.eu>
2020-06-18 23:57:52 +0200

    improved readability

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 let configFile = {
     // jsSource": "https://pifour1-netdata.example.com/dashboard.js",
     // ^^^^^^^^^^ this is actually not used for now
-    "groups": [ {"groupName": "Group 1",
-                "servers":[
-                    {"serverName": "pifour1",
-                    "serverURL": "https://pifour1-netdata.example.com/"},
-                    {"serverName": "pithree1",
-                    "serverURL": "https://pithree1-netdata.example.com/"}]},
-                {"groupName":"Group 2",
-                "servers":[
-                    {"serverName": "pifour2",
-                    "serverURL": "https://pifour2-netdata.example.com/"}]}]
-}
+    groups: [ {groupName: "Group 1",
+                servers:[
+                    {serverName: "pifour1", serverURL: "https://pifour1-netdata.example.com/"},
+                    {serverName: "pithree1", serverURL: "https://pithree1-netdata.example.com/"}
+                ]},
+                {groupName:"Group 2",
+                servers:[
+                    {serverName: "pifour2", serverURL: "https://pifour2-netdata.example.com/"}]
+                }]
+};
